b21e8387608832f6d4ca48c41bc36e309ba86f07
italoferreira38952-max <italo.ferreira38952@alunos.ufersa.edu.br>
2026-09-05 14:36:54 -0300

    teste_botao

--- jogo.c
+++ jogo.c
@@ -5,12 +5,23 @@ int main(void){
     const int altu = 450;
 
     InitWindow(largu , altu , "teste-jogo");
+Rectangle botao={400 ,225 ,200 ,200 };
+int cont = 0;
 
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
+        if(CheckCollisionPointRec(GetMousePosition(),botao) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
+            cont++;
+        }
+        if(IsKeyPressed(KEY_F11)){
+            ToggleFullscreen();
+        }
+
         BeginDrawing();
         ClearBackground(WHITE);
-        DrawText("dark sousls", 200, 200, 50, BLACK);
+        DrawRectangleRec(botao, RED);
+        DrawText("CLIQUE", 310, 300, 20, BLUE);
+        DrawText(TextFormat("%d", cont), 50, 15, 40, DARKBLUE);
         EndDrawing();
     }
 
