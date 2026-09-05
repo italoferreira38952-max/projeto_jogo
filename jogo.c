135ade73974ec6380ea9e959d6ec3157e91c910e
italoferreira38952-max <italo.ferreira38952@alunos.ufersa.edu.br>
2026-09-05 10:22:58 -0300

    tela

--- jogo.c
+++ jogo.c
@@ -1,1 +1,19 @@
 #include "raylib.h"
+int main(void){
+
+    const int largu = 800;
+    const int altu = 450;
+
+    InitWindow(largu , altu , "teste-jogo");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(WHITE);
+        DrawText("dark sousls", 200, 200, 50, BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
