9f5d716d72feb0ab3786e7897c045815cb34adcf
italoferreira38952-max <italo.ferreira38952@alunos.ufersa.edu.br>
2026-09-18 19:14:48 -0300

    status jogador

--- jogo.c
+++ jogo.c
@@ -5,9 +5,16 @@ typedef enum {
     NUM_UPGRADES } TipoUpgrade;
 
 typedef struct {
+    long totalCliques;
+    long cliquesManuais;
+} Estatisticas;
+
+typedef struct {
     char nome[30];
     long moedas;
+    Estatisticas stats;
 } Jogador;
+
 
 int main(void){
 
@@ -19,6 +26,8 @@ int main(void){
 float raio=100.0f;
 Jogador jogador;
 jogador.moedas = 0;
+jogador.stats.totalCliques = 0;
+jogador.stats.cliquesManuais = 0;
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
 
@@ -27,8 +36,12 @@ jogador.moedas = 0;
         Vector2 cbotao = {(float)x /2.0f, (float)y / 2.0f};
         if(CheckCollisionPointCircle(GetMousePosition(),cbotao,raio) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
             jogador.moedas++;
+            jogador.stats.totalCliques++;
+            jogador.stats.cliquesManuais++;
         }if(IsKeyPressed(KEY_SPACE)){
             jogador.moedas++; 
+            jogador.stats.totalCliques++;
+            jogador.stats.cliquesManuais++;
         }
         if(IsKeyPressed(KEY_F11)){
             ToggleFullscreen();
@@ -45,7 +58,8 @@ jogador.moedas = 0;
         DrawRectangle(600,320,170,50,GRAY);
         DrawCircleV(cbotao, raio, RED);
         DrawText("CLIQUE", cbotao.x - 35, cbotao.y - 10, 20, WHITE);
-        DrawText(TextFormat("TOTAL DE CLIQUES: %d", jogador.moedas), 50, 15, 40, DARKBLUE);
+        DrawText(TextFormat("Moedas: %ld", jogador.moedas), 20, 20, 20, DARKBLUE);
+        DrawText(TextFormat("Cliques: %ld", jogador.stats.totalCliques), 20, 50, 20, GRAY);
         //adicionei essa informação
          DrawText("ESPAÇO faz ganhar cliques | ESC sai", 20, y - 25, 26, GRAY);
         EndDrawing();
